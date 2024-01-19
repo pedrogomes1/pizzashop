f241db0c4f9b0eef6111a0b3a776108eedd1b7c7
Pedro Gomes <pedro-gomes@ibm.com>
2024-01-19 15:53:22 -0300

    feat: added orders list by api

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,5 +1,7 @@
+import { useQuery } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 
+import { getOrders } from '@/api/get-orders'
 import { Pagination } from '@/components/pagination'
 import {
   Table,
@@ -13,6 +15,11 @@ import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
 
 export function Orders() {
+  const { data: result } = useQuery({
+    queryKey: ['orders'],
+    queryFn: getOrders,
+  })
+
   return (
     <>
       <Helmet title="Pedidos" />
@@ -38,7 +45,10 @@ export function Orders() {
               </TableHeader>
 
               <TableBody>
-                <OrderTableRow />
+                {result &&
+                  result.orders.map((result) => (
+                    <OrderTableRow order={result} key={result.orderId} />
+                  ))}
               </TableBody>
             </Table>
           </div>
