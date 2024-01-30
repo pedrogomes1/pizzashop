8a2c08422a087a52637d91846bc5e3598d18915f
Pedro Gomes <pedro-gomes@ibm.com>
2024-01-30 14:25:05 -0300

    feat: load orders

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -15,6 +15,7 @@ import {
 
 import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
+import { OrderTableSkeleton } from './order-table-skeleton'
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
@@ -28,7 +29,7 @@ export function Orders() {
     .transform((page) => page - 1)
     .parse(searchParams.get('page') ?? '1')
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ['orders', pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -40,19 +41,19 @@ export function Orders() {
   })
 
   function handlePaginate(pageIndex: number) {
-    setSearchParams((prev) => {
-      prev.set('page', (pageIndex + 1).toString())
+    setSearchParams((state) => {
+      state.set('page', (pageIndex + 1).toString())
 
-      return prev
+      return state
     })
   }
 
   return (
     <>
       <Helmet title="Pedidos" />
+
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-
         <div className="space-y-2.5">
           <OrderTableFilters />
 
@@ -70,24 +71,25 @@ export function Orders() {
                   <TableHead className="w-[132px]"></TableHead>
                 </TableRow>
               </TableHeader>
-
               <TableBody>
                 {result &&
-                  result.orders.map((result) => (
-                    <OrderTableRow order={result} key={result.orderId} />
-                  ))}
+                  result.orders.map((order) => {
+                    return <OrderTableRow key={order.orderId} order={order} />
+                  })}
               </TableBody>
             </Table>
           </div>
+          {isLoadingOrders && <OrderTableSkeleton />}
+
+          {result && (
+            <Pagination
+              onPageChange={handlePaginate}
+              pageIndex={result.meta.pageIndex}
+              totalCount={result.meta.totalCount}
+              perPage={result.meta.perPage}
+            />
+          )}
         </div>
-        {result && (
-          <Pagination
-            onPageChange={handlePaginate}
-            pageIndex={result.meta.pageIndex}
-            totalCount={result.meta.totalCount}
-            perPage={result.meta.perPage}
-          />
-        )}
       </div>
     </>
   )
