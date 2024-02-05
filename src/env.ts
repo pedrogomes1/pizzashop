459ed96fe3d6dac5582ae08c1571cbd528002e33
Pedro Gomes <pedro-gomes@ibm.com>
2024-02-05 10:09:28 -0300

    chore: config msw service worker

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
+  MODE: z.enum(['production', 'development', 'test']),
   VITE_API_URL: z.string().url(),
   VITE_API_ENABLE_DELAY: z.string().transform((value) => value === 'true'),
 })
